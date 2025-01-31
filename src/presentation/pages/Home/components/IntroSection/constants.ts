13ae39106bc0334d59447997ebd78144b1124f7b
Igor Lima <igorlimadrum@gmail.com>
2025-01-30 23:14:07 -0300

    feat: implement gmail presence confirmation

---
+++ src/presentation/pages/Home/components/IntroSection/constants.ts
@@ -0,0 +1,10 @@
+export const EMAIL_TO = import.meta.env.VITE_EMAIL_TO;
+export const EMAIL_SUBJECT = "Que alegria! Minha presença está confirmada! 🎉💍";
+export const EMAIL_BODY = `Queridos Igor e Pâmela,
+    
+    É com imensa alegria que confirmo minha presença nesse dia tão especial! 💖✨
+    
+    Mal posso esperar para celebrar o amor de vocês no dia 30 de agosto e testemunhar esse lindo capítulo que se inicia. Tenho certeza de que será um momento inesquecível, cheio de amor, emoção e felicidade!
+    
+    Com carinho,
+    [Seu Nome Aqui]`;
--- src/presentation/pages/Home/components/IntroSection/index.tsx
+++ src/presentation/pages/Home/components/IntroSection/index.tsx
@@ -1,6 +1,19 @@
+import { Button, HStack } from "@chakra-ui/react";
+import { EMAIL_BODY, EMAIL_SUBJECT, EMAIL_TO } from "./constants";
+import { useOpenGmail } from "./hooks/useOpenGmail";
+
 export function IntroSection() {
+  const { handleOpenGmail } = useOpenGmail({
+    to: EMAIL_TO,
+    subject: EMAIL_SUBJECT,
+    body: EMAIL_BODY,
+  });
+
   return (
     <>
+      <HStack>
+        <Button onClick={handleOpenGmail}>Confirmar presença</Button>
+      </HStack>
     </>
   );
 }
