682f4d893dc9f00c01c4c448c08b9f966676fc4a
Igor Lima <igorlimadrum@gmail.com>
2025-02-04 21:43:49 -0300

    feat: set colors variables

--- src/presentation/theme/tokens/colors/index.ts
+++ src/presentation/theme/tokens/colors/index.ts
@@ -1,1 +1,8 @@
-export const colors = { primary: { value: "#ffffff" } };
+export const colors = {
+  primary: { value: "#ED732F" },
+  secondary: { value: "#2F80ED" },
+  background: { value: "#F7F7F7" },
+  text: { value: "#333333" },
+  muted: { value: "#828282" },
+  accent: { value: "#FFB800" },
+};
