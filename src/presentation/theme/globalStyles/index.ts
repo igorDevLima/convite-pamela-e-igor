ddcc7c718d7e41bbb9019885ba03a9786d8d3085
Igor Lima <igorlimadrum@gmail.com>
2025-02-04 21:44:11 -0300

    style: page viewport

--- src/presentation/theme/globalStyles/index.ts
+++ src/presentation/theme/globalStyles/index.ts
@@ -1,6 +1,12 @@
 export const globalCss = {
   "html, body": {
+    height: "100vh",
+    width: "100vw",
     margin: 0,
     padding: 0,
+  },
+
+  "html, #root, .App": {
+    height: "100%",
   },
 };
