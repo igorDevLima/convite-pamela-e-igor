638e1a85409b8b801b5b71ca60392936573d4502
Igor Lima <igorlimadrum@gmail.com>
2025-02-06 20:22:29 -0300

    feat: show background on scroll page

--- src/presentation/layouts/Home/components/Header/index.tsx
+++ src/presentation/layouts/Home/components/Header/index.tsx
@@ -1,18 +1,35 @@
 import { Box, BoxProps, Image } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 interface HeaderProps extends BoxProps {}
 
 export function Header({ ...rest }: HeaderProps) {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <Box
       as="header"
-      background="{colors.primary}"
       padding={4}
       textAlign="center"
+      position="fixed"
+      width="100%"
+      zIndex={9999}
+      transition="background 0.3s ease"
+      backgroundColor={isScrolled ? "{colors.background}" : "transparent"}
       {...rest}
     >
       <Image src="../../../../../../public/logo.svg" />
-      Header
     </Box>
   );
 }
